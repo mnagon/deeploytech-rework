7016e27dd92a0d76ba86a04eeef2f5634ff2172d
mnagon <mnagon@hotmail.com>
2023-04-24 17:44:29 +0700

    fix: go top link title

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: FC = () => {
         <h2 className='whitespace-pre-line font-prompt text-5xl font-medium dark:text-white'>
           {t('footer__title')}
         </h2>
-        <Link className='text-prompt underline dark:text-white' href='/' title='Go to home page'>
+        <Link className='text-prompt underline dark:text-white' href='/' title='Go to Top'>
           {t('footer__back-to-top__button')}
         </Link>
         <div className='grid gap-8 lg:grid-cols-4'>
